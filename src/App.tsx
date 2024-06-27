324379592d068880757859730ed3f1e89ac00f38
satymshrma <44706382+satymshrma@users.noreply.github.com>
2024-06-27 23:25:03 +0530

    Final setup touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <>react and vite check</>;
+  return (
+    <>
+      <main
+        onContextMenu={(e) => e.preventDefault()}
+        className="p-1 flex-col bg-black text-white h-svh select-none font-mono"
+      >
+        <h1 className="sticky text-center top-0 font-extrabold text-3xl">
+          Hello Vite + React!
+        </h1>
+      </main>
+    </>
+  );
 }
 
 export default App;
